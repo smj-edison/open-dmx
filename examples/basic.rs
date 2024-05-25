b09afd217b506e5b4ca120e143653816d950cd77
Mason Jones <stephen.mason.jones@gmail.com>
2024-05-24 18:17:27 -0700

    Chase effect

--- examples/basic.rs
+++ examples/basic.rs
@@ -17,16 +17,23 @@ fn main() {
     loop {
         channels.fill(0);
 
-        let before = i as usize;
-        let after = before + 1;
+        for sector in 0..8 {
+            let offset = ((i + sector as f32) % 8.0) / 8.0;
 
-        channels[before * 3] = ((1.0 - i.fract()) * 255.0) as u8;
-        channels[after * 3] = (i.fract() * 255.0) as u8;
+            let amount = if offset > 0.5 {
+                (1.0 - offset) * 2.0
+            } else {
+                offset * 2.0
+            };
 
-        dmx.set_channels(channels);
+            let powah = amount.powf(2.2);
 
-        thread::sleep(Duration::from_millis(20));
+            channels[sector * 3 + 0] = (powah * 255.0) as u8;
+        }
 
         i = (i + 0.1) % 8.0;
+
+        dmx.set_channels(channels);
+        thread::sleep(Duration::from_millis(100));
     }
 }
